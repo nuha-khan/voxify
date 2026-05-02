50fd52f496a6f3bcb66c33cc58f02c4cf045ed78
nuha-khan <nuhakhan288@gmail.com>
2026-05-02 19:30:45 +0530

    Create a service
    Service for handling backend and frontend interaction

--- src/app/services/ai.ts
+++ src/app/services/ai.ts
@@ -1,1 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class Ai {
+
+  private apiUrl = 'http://localhost:3000/api/chat';
+
+  constructor(private http: HttpClient) {}
+
+  generateResponse(prompt: string, mode: string) {
+  return this.http.post('http://localhost:3000/api/chat', {
+    prompt: prompt,
+    mode: mode
+  });
+}
+}
