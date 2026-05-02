5177f9c8f657450aea02c37580ad25424726c275
nuha-khan <nuhakhan288@gmail.com>
2026-05-02 19:29:01 +0530

    Create home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,1 +1,34 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
+@Component({
+  selector: 'app-home',
+  imports: [MatButtonModule,MatCardModule,NgFor],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  features = [
+    {
+      title: '📘 Simple Learning',
+      desc: 'Complex election concepts explained easily'
+    },
+    {
+      title: '⚡ Instant Answers',
+      desc: 'Ask anything and get answers instantly'
+    },
+    {
+      title: '🧠 Smart Modes',
+      desc: 'Simple, Detailed, and Step-by-step responses'
+    }
+  ];
+
+  constructor(private router: Router) {}
+
+  goToChat() {
+    this.router.navigate(['/chat']);
+  }
+}
